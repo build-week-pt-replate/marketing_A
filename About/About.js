91f3986e54c48f1876b76cbc3796012ebb821934
Matt Conway <mattconway727@gmail.com>
2019-05-06 21:04:01 -0400

    minor changes

--- About/About.js
+++ About/About.js
@@ -2,12 +2,14 @@ class Article {
   constructor(domElement) {
     this.domElement = domElement;
     this.expandButton = domElement.querySelector(".expandButton");
+    this.image = domElement.querySelector(".pic");
     this.expandButton.textContent = "expand";
     this.expandButton.addEventListener("click", this.expandArticle.bind(this));
   }
 
   expandArticle() {
     this.domElement.classList.toggle("article-open");
+    this.image.classList.toggle("dev-closed");
   }
 }
 
